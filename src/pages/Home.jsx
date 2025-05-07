2c133ce6866ecf293ffa279f2b1c62e0868df666
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-05-07 16:25:15 +0530

    generating stars in background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
+import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme toggle */}
-      <ThemeToggle /> 
+      <ThemeToggle />
       {/* Background effects */}
-
+      <StarBackground />
       {/* Navbar */}
 
       {/* Main content */}
