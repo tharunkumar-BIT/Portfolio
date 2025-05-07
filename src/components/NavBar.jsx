1aa11f6588b3f3cfb6de1133d3fa7855f05de011
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-05-07 20:23:20 +0530

    mobile navbar design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,6 +27,33 @@ export const NavBar = () => {
         "fixed w-full z-40 transition-all duration-300",
         isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
       )}
-    ></nav>
+    >
+      <div className="container flex items-center justify-between">
+        <a
+          className="text-xl font-bold text-primary flex items-center"
+          href="#hero"
+        >
+          <span className="relative z-10">
+            <span className="text-glow text-foreground">Tharunkumar's</span>{" "}
+            Portfolio
+          </span>
+        </a>
+
+        {/* desktop navbar */}
+        <div className="hidden md:flex space-x-8">
+          {navItems.map((item, key) => (
+            <a
+              key={key}
+              href={item.href}
+              className="text-foreground/80 hover:text-primary transition-colors duration-300"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+
+        {/* mobile navbar */}
+      </div>
+    </nav>
   );
 };
