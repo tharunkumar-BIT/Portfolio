6fb8522976c1e11542596ec0f25f45424b1edb00
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-05-07 09:26:33 +0530

    defining routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
 
+function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
