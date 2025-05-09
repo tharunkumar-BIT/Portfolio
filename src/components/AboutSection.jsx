e80388a4f04ab6feac263b3ba6a23f803a99a62e
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-05-09 12:11:07 +0530

    implement download cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href=""
+                href="https://drive.google.com/uc?export=download&id=1N6s3zQ0CmsbxU4KryYgvLEU703c9MKCa" download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
